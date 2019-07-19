1d1db3eb3ccff821e7d466057ccfe6a217edfee5
Peyton Doyle <p6doyle@gmail.com>
2019-07-19 14:02:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash["apples"]
-    hash["apples"] += 1
+  if hash[key]
+    hash[key] += 1
   else
-    hash["apples"] = 1
+    hash[key] = 1
 end
 
 
