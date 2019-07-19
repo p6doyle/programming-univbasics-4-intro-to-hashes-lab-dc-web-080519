f806e319906620c2ee689b5a2d59011629070c81
Peyton Doyle <p6doyle@gmail.com>
2019-07-19 13:54:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-    read_from_hash = {:person => "Tzvi"}
+    read_from_hash = {:person => "Steve"}
     read_from_hash[key]
 end
 
