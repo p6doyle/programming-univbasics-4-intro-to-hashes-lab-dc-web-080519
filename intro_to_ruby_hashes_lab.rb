595dea93f2591dbfcf64857794528ddfede76b1c
Peyton Doyle <p6doyle@gmail.com>
2019-07-19 13:17:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,21 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {:names => "Peyton", :age => "28", :sign => "Taurus"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {:name => "Grace Hopper"} 
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  idgenerator = {:id => 24}
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator[:name] = "Grace Hopper"
 end
 
 def read_from_hash(hash, key)
